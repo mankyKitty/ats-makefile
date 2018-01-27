3d72f247e8e87b474339c5655dc84cf176c87849
Vanessa McHale <vanessa.mchale@reconfigure.io>
2018-01-26 18:23:20 -0600

    scripts & such.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-wget http://ats-lang.sourceforge.net/IMPLEMENT/Postiats/ATS2-Postiats-0.3.9.tgz
+wget https://downloads.sourceforge.net/project/ats2-lang/ats2-lang/ats2-postiats-0.3.9/ATS2-Postiats-0.3.9.tgz
 tar xvf ATS2-Postiats-0.3.9.tgz
 cd ATS2-Postiats-0.3.9 && ./configure && make && sudo make install
 cd ../ || exit
